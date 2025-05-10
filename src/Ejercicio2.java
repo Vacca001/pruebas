a64fa6b1fd42bc7fcade93b5a1a4c5256352233d
Vacca001 <133098352+Vacca001@users.noreply.github.com>
2025-05-10 12:21:03 -0500

    Add files via upload

---
+++ src/Ejercicio2.java
@@ -0,0 +1,20 @@
+
+
+    class Persona {
+        String nombre;
+
+    }
+
+     public class Ejercicio2 {
+        public static void main(String[] args) {
+            Persona persona = new Persona();
+            persona.nombre = "Ana";
+            cambiarNombre(persona);
+            System.out.println("Nombre después de cambiarNombre: " + persona.nombre); 
+        }
+
+        public static void cambiarNombre(Persona persona) {
+            persona.nombre = "María";
+        }
+    }
+
